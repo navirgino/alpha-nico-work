c9873b3f1d7113f27e3bb814e8ca2c6d76134317
Nico Virgino <nicovirgino@gmail.com>
2022-08-05 22:42:12 -0500

    deleting cringe files

--- testing practicing.py
+++
@@ -1,7 +0,0 @@
-name = input("What yo name is mamacita bonita? ")
-age = int(input("Oyeaaa befoe i fuhget how old u iz? "))
-print("Aight so wat u tryna tell me is that u want me right?")
-
-print('do not play these games with me', name,)
-print('you deadass a ripe', age)
-
